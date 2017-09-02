20684131f5449c3f4d35d2acee255508d418838f
Zhunissali <zshanabek@gmail.com>
2017-09-02 22:15:10 +0600

    test

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,6 +2,6 @@ class Subject < ApplicationRecord
   reverse_geocoded_by :latitude, :longitude
   after_validation :reverse_geocode  # auto-fetch address
   has_many :attendances, dependent: :destroy
-  validates :location, :longitude, :radius, presence: true
-  
+  # validates :location, :longitude, :radius, presence: true
+
 end
