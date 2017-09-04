179cac4ac6627d4d3b03b5d351306d31f912d529
Zhunisali <zshanabek@gmail.com>
2017-09-05 02:15:21 +0600

    styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
   resources :subjects do
     resources :attendances, only: [:index, :show, :create]
   end
 
-  authenticated :user do
-    root 'subjects#index', as: :authenticated_root
-  end
-
-  root controller: 'welcome', action: :index  
+  root controller: 'welcome', action: :index
   post '/SubjectCreate', to: 'subject#create'
 end
