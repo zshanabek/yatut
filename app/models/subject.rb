a7f8dae9563f0f2ee46b69537c74f9b3bde653f5
Zhunisali <zshanabek@gmail.com>
2017-09-06 21:44:34 +0600

    test

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -8,7 +8,5 @@ class Subject < ApplicationRecord
   private
 	  def time_has_a_date
 	    self.start_time = self.start_time.change(year: self.date.year, day: self.date.day, month: self.date.month)
-	    self.end_time = DateTime.parse('1998-02-03T04:05:06+07:00')
-	    self.created_at = DateTime.parse('1988-02-03T04:05:06+07:00')
 	  end
 end
